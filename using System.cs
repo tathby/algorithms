bae68409487a9d2de8909ad003dd11d2f184f571
tathby <cross002@colum.edu>
2025-10-04 17:44:24 -0500

    add fisher yates algorithm

---
+++ using System.cs
@@ -0,0 +1,19 @@
+using System.IO;
+using System;
+
+
+string[] FishyGrapesShuffle(string path)
+{
+
+    Random ran = new Random();
+    string[] data = File.ReadAllLines(path);
+    int n = data.Length;
+    for (int i = n - 1; i > 0; i--)
+    {
+        int j = ran.Next(i + 1);
+        string tmp = data[i];
+        data[i] = data[j];
+        data[j] = tmp;
+    }
+    return data;
+}
